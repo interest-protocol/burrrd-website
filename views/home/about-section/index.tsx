d387a948cfab53549d621a9dd066041154572dbe
Atila <atilaesousa@gmail.com>
2023-12-21 13:24:20 +0100

    style: text color changed

--- views/home/about-section/index.tsx
+++ views/home/about-section/index.tsx
@@ -151,13 +151,13 @@ const AboutSection: FC = () => {
                   }
                   lineHeight={['1.5rem', '1.5rem', '1.5rem', '2rem']}
                 >
-                  <Typography variant="medium">
+                  <Typography variant="medium" color="white">
                     But BURRRD inst just on a mission to eliminate the rubbish;
                     he has a bigger plan in mind. As he soars through the crypto
                     skies, the feathered hero is on a mission to take everyone
                     to the moon with his memetic power.
                   </Typography>
-                  <Typography variant="medium">
+                  <Typography variant="medium" color="white">
                     So, watch out, world! BURRRD is armed and ready to bring a
                     slice of justice to the crypto realm. Prepare to witness the
                     epic rise of the one and only BURRRD, as he carves his way
