932a3518baaaba4a0898523c84ae9f76a390125e
Jose Cerqueira <josecerqueira@protonmail.com>
2024-01-04 12:38:08 +0000

    feat: hide zebec

--- views/home/partners/partners.data.ts
+++ views/home/partners/partners.data.ts
@@ -11,9 +11,9 @@ export const PARTNERS = [
     img: '/partners/stoned-ape-crew.png',
     post: 'https://twitter.com/StonedApeCrew/status/1741135499215908897',
   },
-  {
-    name: 'Zebec',
-    link: 'https://zebec.io/',
-    img: '/partners/zebec.jpeg',
-  },
+  // {
+  //   name: 'Zebec',
+  //   link: 'https://zebec.io/',
+  //   img: '/partners/zebec.jpeg',
+  // },
 ];
