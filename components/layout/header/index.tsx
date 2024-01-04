11e8eb7dcf9313cca4395a797168bf7308ce7154
git-marcopitra <marcomorete73@hotmail.com>
2024-01-04 19:47:43 +0100

    fix: graph and add shib

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -1,5 +1,5 @@
 import { Box, Motion, Typography } from '@interest-protocol/ui-kit';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { FC, useCallback, useState } from 'react';
 
 import { useTheme } from '@/context/theme-context';
@@ -10,8 +10,9 @@ import MobileNavbar from './mobile-navbar';
 import Navbar from './navbar';
 
 const Header: FC = () => {
-  const [isMobile, setIsMobile] = useState(false);
   const { theme } = useTheme();
+  const { push } = useRouter();
+  const [isMobile, setIsMobile] = useState(false);
 
   const handleSetDesktop = useCallback(() => {
     const mediaIsMobile = !window.matchMedia('(min-width: 62em)').matches;
@@ -28,98 +29,98 @@ const Header: FC = () => {
         alignItems="center"
         justifyContent="space-between"
       >
-        <Link href="/">
-          {!isMobile ? (
-            <Box
-              gap="1rem"
-              display="flex"
-              width="14.375rem"
-              position="relative"
-              alignItems="center"
+        {!isMobile ? (
+          <Box
+            gap="1rem"
+            display="flex"
+            width="14.375rem"
+            position="relative"
+            alignItems="center"
+            onClick={() => push('/')}
+          >
+            <Motion
+              animate={{
+                x: [-60, -60, -100, 0],
+                rotate: [
+                  50, 50, 50, 50, 50, 50, 50, 50, 20, -20, 20, -20, 20, -20, 20,
+                  -20, 20, 0,
+                ],
+              }}
+              position="absolute"
+              initial={{ x: -60, rotate: 50 }}
+              transition={{ duration: 3.5 }}
+              width="3.5rem"
             >
-              <Motion
-                animate={{
-                  x: [-60, -60, -100, 0],
-                  rotate: [
-                    50, 50, 50, 50, 50, 50, 50, 50, 20, -20, 20, -20, 20, -20,
-                    20, -20, 20, 0,
-                  ],
-                }}
-                position="absolute"
-                initial={{ x: -60, rotate: 50 }}
-                transition={{ duration: 3.5 }}
-                width="3.5rem"
-              >
-                <BurrrdSVG
-                  width="100%"
-                  maxWidth="12.625rem"
-                  maxHeight="3.5625rem"
-                />
-              </Motion>
-              <Motion
-                right="0"
-                animate={{ x: 0 }}
-                position="absolute"
-                initial={{ x: -300 }}
-                transition={{ duration: 2 }}
-              >
-                <Typography
-                  margin="0"
-                  color="white"
-                  variant="medium"
-                  fontWeight="700"
-                  fontFamily="Asap"
-                  fontSize="2.5rem"
-                  textTransform="capitalize"
-                >
-                  BURRRD
-                </Typography>
-              </Motion>
-            </Box>
-          ) : (
-            <Box
-              gap="1.25rem"
-              display="flex"
-              height="0.625rem"
-              position="relative"
-              alignItems="center"
+              <BurrrdSVG
+                width="100%"
+                maxWidth="12.625rem"
+                maxHeight="3.5625rem"
+              />
+            </Motion>
+            <Motion
+              right="0"
+              animate={{ x: 0 }}
+              position="absolute"
+              initial={{ x: -300 }}
+              transition={{ duration: 2 }}
             >
-              <Motion
-                animate={{
-                  x: [-60, -60, -100, 0],
-                  rotate: [
-                    50, 50, 50, 50, 50, 50, 50, 50, 20, -20, 20, -20, 20, -20,
-                    20, -20, 20, 0,
-                  ],
-                }}
-                initial={{ x: -60, rotate: 50 }}
-                transition={{ duration: 3.5 }}
+              <Typography
+                margin="0"
+                color="white"
+                variant="medium"
+                fontWeight="700"
+                fontFamily="Asap"
+                fontSize="2.5rem"
+                textTransform="capitalize"
               >
-                <BurrrdSVG
-                  width="1.75rem"
-                  maxWidth="12.625rem"
-                  maxHeight="3.5625rem"
-                />
-              </Motion>
-              <Motion
-                initial={{ x: -300 }}
-                animate={{ x: 0 }}
-                transition={{ duration: 2 }}
+                BURRRD
+              </Typography>
+            </Motion>
+          </Box>
+        ) : (
+          <Box
+            gap="1.25rem"
+            display="flex"
+            height="0.625rem"
+            position="relative"
+            alignItems="center"
+            onClick={() => push('/')}
+          >
+            <Motion
+              animate={{
+                x: [-60, -60, -100, 0],
+                rotate: [
+                  50, 50, 50, 50, 50, 50, 50, 50, 20, -20, 20, -20, 20, -20, 20,
+                  -20, 20, 0,
+                ],
+              }}
+              initial={{ x: -60, rotate: 50 }}
+              transition={{ duration: 3.5 }}
+            >
+              <BurrrdSVG
+                width="1.75rem"
+                maxWidth="12.625rem"
+                maxHeight="3.5625rem"
+              />
+            </Motion>
+            <Motion
+              initial={{ x: -300 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 2 }}
+            >
+              <Typography
+                color="white"
+                variant="medium"
+                fontWeight="700"
+                fontFamily="Asap"
+                fontSize="1.5rem"
+                textTransform="capitalize"
               >
-                <Typography
-                  color="white"
-                  variant="medium"
-                  fontWeight="700"
-                  fontFamily="Asap"
-                  fontSize="1.5rem"
-                  textTransform="capitalize"
-                >
-                  BURRRD
-                </Typography>
-              </Motion>
-            </Box>
-          )}
-        </Link>
+                BURRRD
+              </Typography>
+            </Motion>
+          </Box>
+        )}
         <Navbar />
         <MobileNavbar />
       </Box>
