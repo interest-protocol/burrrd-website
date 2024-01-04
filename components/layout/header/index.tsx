8aed5c8525df90333ae01e18ac59d08c4dc508d9
git-marcopitra <marcomorete73@hotmail.com>
2024-01-04 19:01:33 +0100

    fix: logo route to home

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Motion, Typography } from '@interest-protocol/ui-kit';
+import Link from 'next/link';
 import { FC, useCallback, useState } from 'react';
 
 import { useTheme } from '@/context/theme-context';
@@ -27,96 +28,98 @@ const Header: FC = () => {
         alignItems="center"
         justifyContent="space-between"
       >
-        {!isMobile ? (
-          <Box
-            gap="1rem"
-            display="flex"
-            width="14.375rem"
-            position="relative"
-            alignItems="center"
-          >
-            <Motion
-              animate={{
-                x: [-60, -60, -100, 0],
-                rotate: [
-                  50, 50, 50, 50, 50, 50, 50, 50, 20, -20, 20, -20, 20, -20, 20,
-                  -20, 20, 0,
-                ],
-              }}
-              position="absolute"
-              initial={{ x: -60, rotate: 50 }}
-              transition={{ duration: 3.5 }}
-              width="3.5rem"
+        <Link href="/">
+          {!isMobile ? (
+            <Box
+              gap="1rem"
+              display="flex"
+              width="14.375rem"
+              position="relative"
+              alignItems="center"
             >
-              <BurrrdSVG
-                width="100%"
-                maxWidth="12.625rem"
-                maxHeight="3.5625rem"
-              />
-            </Motion>
-            <Motion
-              right="0"
-              animate={{ x: 0 }}
-              position="absolute"
-              initial={{ x: -300 }}
-              transition={{ duration: 2 }}
-            >
-              <Typography
-                margin="0"
-                color="white"
-                variant="medium"
-                fontWeight="700"
-                fontFamily="Asap"
-                fontSize="2.5rem"
-                textTransform="capitalize"
+              <Motion
+                animate={{
+                  x: [-60, -60, -100, 0],
+                  rotate: [
+                    50, 50, 50, 50, 50, 50, 50, 50, 20, -20, 20, -20, 20, -20,
+                    20, -20, 20, 0,
+                  ],
+                }}
+                position="absolute"
+                initial={{ x: -60, rotate: 50 }}
+                transition={{ duration: 3.5 }}
+                width="3.5rem"
               >
-                BURRRD
-              </Typography>
-            </Motion>
-          </Box>
-        ) : (
-          <Box
-            gap="1.25rem"
-            display="flex"
-            height="0.625rem"
-            position="relative"
-            alignItems="center"
-          >
-            <Motion
-              animate={{
-                x: [-60, -60, -100, 0],
-                rotate: [
-                  50, 50, 50, 50, 50, 50, 50, 50, 20, -20, 20, -20, 20, -20, 20,
-                  -20, 20, 0,
-                ],
-              }}
-              initial={{ x: -60, rotate: 50 }}
-              transition={{ duration: 3.5 }}
-            >
-              <BurrrdSVG
-                width="1.75rem"
-                maxWidth="12.625rem"
-                maxHeight="3.5625rem"
-              />
-            </Motion>
-            <Motion
-              initial={{ x: -300 }}
-              animate={{ x: 0 }}
-              transition={{ duration: 2 }}
-            >
-              <Typography
-                color="white"
-                variant="medium"
-                fontWeight="700"
-                fontFamily="Asap"
-                fontSize="1.5rem"
-                textTransform="capitalize"
+                <BurrrdSVG
+                  width="100%"
+                  maxWidth="12.625rem"
+                  maxHeight="3.5625rem"
+                />
+              </Motion>
+              <Motion
+                right="0"
+                animate={{ x: 0 }}
+                position="absolute"
+                initial={{ x: -300 }}
+                transition={{ duration: 2 }}
               >
-                BURRRD
-              </Typography>
-            </Motion>
-          </Box>
-        )}
+                <Typography
+                  margin="0"
+                  color="white"
+                  variant="medium"
+                  fontWeight="700"
+                  fontFamily="Asap"
+                  fontSize="2.5rem"
+                  textTransform="capitalize"
+                >
+                  BURRRD
+                </Typography>
+              </Motion>
+            </Box>
+          ) : (
+            <Box
+              gap="1.25rem"
+              display="flex"
+              height="0.625rem"
+              position="relative"
+              alignItems="center"
+            >
+              <Motion
+                animate={{
+                  x: [-60, -60, -100, 0],
+                  rotate: [
+                    50, 50, 50, 50, 50, 50, 50, 50, 20, -20, 20, -20, 20, -20,
+                    20, -20, 20, 0,
+                  ],
+                }}
+                initial={{ x: -60, rotate: 50 }}
+                transition={{ duration: 3.5 }}
+              >
+                <BurrrdSVG
+                  width="1.75rem"
+                  maxWidth="12.625rem"
+                  maxHeight="3.5625rem"
+                />
+              </Motion>
+              <Motion
+                initial={{ x: -300 }}
+                animate={{ x: 0 }}
+                transition={{ duration: 2 }}
+              >
+                <Typography
+                  color="white"
+                  variant="medium"
+                  fontWeight="700"
+                  fontFamily="Asap"
+                  fontSize="1.5rem"
+                  textTransform="capitalize"
+                >
+                  BURRRD
+                </Typography>
+              </Motion>
+            </Box>
+          )}
+        </Link>
         <Navbar />
         <MobileNavbar />
       </Box>
