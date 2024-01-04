7a357343da178249b33e4cc25461007643dc31cc
git-marcopitra <marcomorete73@hotmail.com>
2024-01-04 13:34:43 +0100

    feat: add song

--- views/chart/chart-graph.tsx
+++ views/chart/chart-graph.tsx
@@ -1,5 +1,5 @@
 import { Box } from '@interest-protocol/ui-kit';
-import { FC } from 'react';
+import { FC, useEffect, useRef } from 'react';
 import {
   Bar,
   BarChart,
@@ -25,6 +25,7 @@ import { formatNumber } from '@/utils';
 import { COINS_IMAGE_MAP } from './chart.data';
 
 const ChartGraph: FC = () => {
+  const audioRef = useRef<HTMLAudioElement>();
   const { data } = useSWR(
     'tokens-price',
     async () => {
@@ -58,7 +59,13 @@ const ChartGraph: FC = () => {
 
   const burrrdNextMemeAddress = data?.[burrrdIndex - 1]?.address ?? '';
 
-  const deadMemes = data?.slice(burrrdIndex).map(({ address }) => address);
+  const deadMemes = data?.slice(burrrdIndex + 1).map(({ address }) => address);
+
+  useEffect(() => {
+    const audio = new Audio();
+    audio.src = '/sound/quack_stab_stab.mp3';
+    audioRef.current = audio;
+  }, []);
 
   return (
     <>
@@ -105,102 +112,115 @@ const ChartGraph: FC = () => {
         flexDirection={['column', 'column', 'row']}
         alignItems={['center', 'center', 'flex-end']}
       >
-        <Box minWidth="15rem" width="15rem" height="18rem" position="relative">
-          {deadMemes?.includes(NAMES_TO_ADDRESS.USEDCAR) && (
-            <Box position="absolute" top="0">
-              <img
-                src="/images/dead-coins/USEDCAR-min.webp"
-                alt="Dead USEDCAR"
-                width="250"
-              />
-            </Box>
-          )}
-          {deadMemes?.includes(NAMES_TO_ADDRESS.BONK) && (
-            <Box position="absolute" bottom="4rem">
-              <img
-                src="/images/dead-coins/BONK-min.webp"
-                alt="Dead BONK"
-                width="100"
-              />
-            </Box>
-          )}
-          {deadMemes?.includes(NAMES_TO_ADDRESS.CHONKY) && (
-            <Box position="absolute" left="4rem" bottom="4rem">
-              <img
-                src="/images/dead-coins/CHONKY-min.webp"
-                alt="Dead CHONKY"
-                width="100"
-              />
-            </Box>
-          )}
-          {deadMemes?.includes(NAMES_TO_ADDRESS.GECKO) && (
-            <Box position="absolute" bottom="4rem" right="0">
-              <img
-                src="/images/dead-coins/GECKO-min.webp"
-                alt="Dead GECKO"
-                width="100"
-              />
-            </Box>
-          )}
-          {deadMemes?.includes(NAMES_TO_ADDRESS.DOGE) && (
-            <Box position="absolute" bottom="2rem">
-              <img
-                src="/images/dead-coins/DODGE-min.webp"
-                alt="Dead DOGE"
-                width="100"
-              />
-            </Box>
-          )}
-          {deadMemes?.includes(NAMES_TO_ADDRESS.MYRO) && (
-            <Box position="absolute" bottom="2rem" left="4.5rem">
-              <img
-                src="/images/dead-coins/DOGWITHAT-min.webp"
-                alt="Dead DOGWIFTHAT"
-                width="100"
-              />
-            </Box>
-          )}
-          {deadMemes?.includes(NAMES_TO_ADDRESS.BONK) && (
-            <Box position="absolute" bottom="2rem" right="0">
-              <img
-                src="/images/dead-coins/MYRO-min.webp"
-                alt="Dead MYRO"
-                width="100"
-              />
-            </Box>
-          )}
-          {deadMemes?.includes(NAMES_TO_ADDRESS.PEPE) && (
-            <Box position="absolute" bottom="0">
-              <img
-                src="/images/dead-coins/PEPE-min.webp"
-                alt="Dead PEPE"
-                width="100"
-              />
-            </Box>
-          )}
-          {deadMemes?.includes(NAMES_TO_ADDRESS.PONKE) && (
-            <Box position="absolute" bottom="0" left="4.5rem">
-              <img
-                src="/images/dead-coins/PONKE-min.webp"
-                alt="Dead PONKE"
-                width="100"
-              />
-            </Box>
-          )}
-          {deadMemes?.includes(NAMES_TO_ADDRESS.SAMO) && (
-            <Box position="absolute" bottom="0" right="0">
-              <img
-                src="/images/dead-coins/SAMO-min.webp"
-                alt="Dead SAMO"
-                width="100"
-              />
-            </Box>
-          )}
-        </Box>
+        {console.log({ deadMemes })}
+        {deadMemes?.length && (
+          <Box
+            width="15rem"
+            height="18rem"
+            minWidth="15rem"
+            position="relative"
+            onClick={() => audioRef.current?.play()}
+          >
+            {deadMemes?.includes(NAMES_TO_ADDRESS.USEDCAR) && (
+              <Box position="absolute" top="0">
+                <img
+                  src="/images/dead-coins/USEDCAR-min.webp"
+                  alt="Dead USEDCAR"
+                  width="250"
+                />
+              </Box>
+            )}
+            {deadMemes?.includes(NAMES_TO_ADDRESS.BONK) && (
+              <Box position="absolute" bottom="4rem">
+                <img
+                  src="/images/dead-coins/BONK-min.webp"
+                  alt="Dead BONK"
+                  width="100"
+                />
+              </Box>
+            )}
+            {deadMemes?.includes(NAMES_TO_ADDRESS.CHONKY) && (
+              <Box position="absolute" left="4rem" bottom="4rem">
+                <img
+                  src="/images/dead-coins/CHONKY-min.webp"
+                  alt="Dead CHONKY"
+                  width="100"
+                />
+              </Box>
+            )}
+            {deadMemes?.includes(NAMES_TO_ADDRESS.GECKO) && (
+              <Box position="absolute" bottom="4rem" right="0">
+                <img
+                  src="/images/dead-coins/GECKO-min.webp"
+                  alt="Dead GECKO"
+                  width="100"
+                />
+              </Box>
+            )}
+            {deadMemes?.includes(NAMES_TO_ADDRESS.DOGE) && (
+              <Box position="absolute" bottom="2rem">
+                <img
+                  src="/images/dead-coins/DODGE-min.webp"
+                  alt="Dead DOGE"
+                  width="100"
+                />
+              </Box>
+            )}
+            {deadMemes?.includes(NAMES_TO_ADDRESS.MYRO) && (
+              <Box position="absolute" bottom="2rem" left="4.5rem">
+                <img
+                  src="/images/dead-coins/DOGWITHAT-min.webp"
+                  alt="Dead DOGWIFTHAT"
+                  width="100"
+                />
+              </Box>
+            )}
+            {deadMemes?.includes(NAMES_TO_ADDRESS.BONK) && (
+              <Box position="absolute" bottom="2rem" right="0">
+                <img
+                  src="/images/dead-coins/MYRO-min.webp"
+                  alt="Dead MYRO"
+                  width="100"
+                />
+              </Box>
+            )}
+            {deadMemes?.includes(NAMES_TO_ADDRESS.PEPE) && (
+              <Box position="absolute" bottom="0">
+                <img
+                  src="/images/dead-coins/PEPE-min.webp"
+                  alt="Dead PEPE"
+                  width="100"
+                />
+              </Box>
+            )}
+            {deadMemes?.includes(NAMES_TO_ADDRESS.PONKE) && (
+              <Box position="absolute" bottom="0" left="4.5rem">
+                <img
+                  src="/images/dead-coins/PONKE-min.webp"
+                  alt="Dead PONKE"
+                  width="100"
+                />
+              </Box>
+            )}
+            {deadMemes?.includes(NAMES_TO_ADDRESS.SAMO) && (
+              <Box position="absolute" bottom="0" right="0">
+                <img
+                  src="/images/dead-coins/SAMO-min.webp"
+                  alt="Dead SAMO"
+                  width="100"
+                />
+              </Box>
+            )}
+          </Box>
+        )}
         <Box display="flex" alignItems="flex-end">
           {burrrdNextMemeAddress ? (
             <>
-              <Box maxHeight="20rem" maxWidth="20rem">
+              <Box
+                maxWidth="20rem"
+                maxHeight="20rem"
+                onClick={() => audioRef.current?.play()}
+              >
                 <RunningBurrrdSVG
                   width="100%"
                   maxWidth="100%"
