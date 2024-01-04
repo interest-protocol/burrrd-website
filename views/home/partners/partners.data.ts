4943d3cd81b30e16d70c77ece67fad6383abcb49
git-marcopitra <marcomorete73@hotmail.com>
2024-01-04 12:55:22 +0100

    fix: add baby biz post

--- views/home/partners/partners.data.ts
+++ views/home/partners/partners.data.ts
@@ -3,6 +3,7 @@ export const PARTNERS = [
     name: 'Monkey Baby Business',
     link: 'https://twitter.com/MonkeyBabyBiz',
     img: '/partners/monkey-baby-businezz.png',
+    post: 'https://twitter.com/BURRRD/status/1742626159248990479',
   },
   {
     name: 'Stoned Ape Crew',
