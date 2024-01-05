a7d25f6e6e8a26bceddb8699412f137dc84c8035
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2024-01-05 01:48:56 +0100

    ♻️  change items (#35)

--- views/home/collaborations/collaborators.data.ts
+++ views/home/collaborations/collaborators.data.ts
@@ -2,24 +2,24 @@ export const COLLABORATORS = [
   {
     name: 'CETS',
     link: 'https://cets.wtf/',
-    img: '/collabs/cets.jpg',
+    img: '/images/collabs/cets.jpg',
     post: 'https://twitter.com/cetsfk/status/1740417421360365913',
   },
   {
     name: 'Namaste',
     link: 'https://twitter.com/SenseiGifts',
-    img: '/collabs/namaste.jpeg',
+    img: '/images/collabs/namaste.jpeg',
     post: 'https://twitter.com/SenseiGifts/status/1740090942898811246',
   },
   {
-    name: 'Stoned Ape Crew',
-    link: 'https://www.stonedapecrew.com/',
-    img: '/collabs/stoned-ape-crew.png',
-    post: 'https://twitter.com/StonedApeCrew/status/1741135499215908897',
+    name: 'Monkey Baby Business',
+    link: 'https://twitter.com/MonkeyBabyBiz',
+    img: '/images/collabs/monkey-baby-businezz.png',
+    post: 'https://twitter.com/BURRRD/status/1742626159248990479',
   },
   {
     name: 'Tombstones',
     link: 'https://www.tombstoned.app/',
-    img: '/collabs/tombstones.jpeg',
+    img: '/images/collabs/tombstones.jpeg',
   },
 ];
