8f26a3a33918f967a8a059cbe816bfadba87945f
MarioBatalha <mariobatalha9@gmail.com>
2024-01-24 12:56:43 +0200

    feat: add new UI

---
+++ components/svg/plus.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+
+import { SVGProps } from './svg.types';
+
+const PlusSVG: FC<SVGProps> = ({ maxHeight, maxWidth, ...props }) => (
+  <svg
+    style={{ maxHeight, maxWidth }}
+    viewBox="0 0 24 24"
+    fill="none"
+    {...props}
+  >
+    <path d="M13 4V11H20V13H13V20H11V13H4V11H11V4H13Z" fill="black" />
+  </svg>
+);
+
+export default PlusSVG;
