2d28cc1ce8544fbe6e055a2af6e2d00e8386fb92
Atila <atilaesousa@gmail.com>
2024-01-24 01:34:33 +0100

    feat: new collaborations section added

---
+++ views/home/collaborations/card/index.tsx
@@ -0,0 +1,36 @@
+import { Box } from '@interest-protocol/ui-kit';
+import { FC } from 'react';
+
+import { CardProps } from './card.types';
+import CardHeader from './header';
+
+const Card: FC<CardProps> = ({ name, img, link }) => (
+  <Box
+    p="1.5rem"
+    bg="#fff"
+    maxWidth="26rem"
+    border="2px solid"
+    borderColor="#000"
+    borderRadius="1rem"
+    minWidth="19.125rem"
+    transform="scale(1)"
+    boxShadow="12px 12px 0px 0px #000"
+    nHover={{ transform: 'scale(1.02)' }}
+    transition="transform 300ms ease-in-out"
+  >
+    <CardHeader name={name} link={link} />
+    <Box
+      mx="auto"
+      mt="1.53rem"
+      width="11.25rem"
+      overflow="hidden"
+      border="2px solid #000"
+      height="11.25rem"
+      borderRadius="50%"
+    >
+      <img alt={name} width="100%" src={img} />
+    </Box>
+  </Box>
+);
+
+export default Card;
