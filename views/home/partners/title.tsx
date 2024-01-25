a55a2a18089d5dbecc72d0ef2394d53c35f1661b
KipandaJr <antoniokipandacardoso@gmail.com>
2024-01-26 00:37:29 +0100

    fix: :bug:  animation fixed

--- views/home/partners/title.tsx
+++ views/home/partners/title.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@interest-protocol/ui-kit';
+import { Box, Motion, Typography } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 
 import { HandsSVG } from '@/svg';
@@ -26,9 +26,23 @@ const Title: FC = () => (
     >
       Partners
     </Typography>
-    <Box position="absolute" top="-5rem" width="9rem" right="-2rem">
+    <Motion
+      position="absolute"
+      animate={{
+        rotate: [0, 30, 0],
+        scale: [1, 0.5, 1, 0.4, 1],
+      }}
+      transition={{
+        duration: 2.5,
+        repeat: Infinity,
+        repeatDelay: 1,
+      }}
+      top="-5rem"
+      width="9rem"
+      right="-2rem"
+    >
       <HandsSVG maxHeight="100%" maxWidth="100%" width="100%" height="60%" />
-    </Box>
+    </Motion>
   </Box>
 );
 
