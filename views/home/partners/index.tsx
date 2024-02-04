c55150837b08a8f53b8546fe106434145356f757
git-marcopitra <marcomorete73@hotmail.com>
2024-02-04 16:11:45 +0100

    fix: partners card

--- views/home/partners/index.tsx
+++ views/home/partners/index.tsx
@@ -7,31 +7,34 @@ import { PARTNERS } from './partners.data';
 import Title from './title';
 
 const Partners: FC = () => (
-  <Box p="6rem 0.75rem 8rem 0.75rem" width="fit-content" mx="auto">
-    <Box
-      gap="2rem"
-      display="grid"
-      justifyContent="center"
-      gridTemplateColumns={['1fr', '1fr', '1fr', '1fr 1fr 1fr']}
-    >
-      {PARTNERS.map((partner, index) => (
-        <Box key={v4()}>
-          {(index == 0 || index == 1) && (
-            <Box
-              display={[
-                index == 0 ? 'block' : 'none',
-                index == 0 ? 'block' : 'none',
-                index == 0 ? 'block' : 'none',
-                index == 1 ? 'block' : 'none',
-              ]}
-            >
-              <Title />
-            </Box>
-          )}
-          <Card {...partner} />
-        </Box>
-      ))}
-    </Box>
+  <Box
+    py="6rem"
+    px="3rem"
+    mx="auto"
+    gap="2rem"
+    width="100%"
+    display="grid"
+    maxWidth="82.5rem"
+    justifyContent="center"
+    gridTemplateColumns={['1fr', '1fr', '1fr', '1fr 1fr 1fr']}
+  >
+    {PARTNERS.map((partner, index) => (
+      <Box key={v4()}>
+        {(index == 0 || index == 1) && (
+          <Box
+            display={[
+              index == 0 ? 'block' : 'none',
+              index == 0 ? 'block' : 'none',
+              index == 0 ? 'block' : 'none',
+              index == 1 ? 'block' : 'none',
+            ]}
+          >
+            <Title />
+          </Box>
+        )}
+        <Card {...partner} />
+      </Box>
+    ))}
   </Box>
 );
 
